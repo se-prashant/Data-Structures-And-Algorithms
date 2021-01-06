0f90a9c1422c9e80c9cf53ab4ac2c0b44ef6b49b
PRASHANT SINGH <prashant791singh@gmail.com>
2021-01-06 23:27:50 +0530

    Update Find the duplicate in an array of N+1 integers.cpp

--- Arrays/Find the duplicate in an array of N+1 integers.cpp
+++ Arrays/Find the duplicate in an array of N+1 integers.cpp
@@ -1,1 +1,19 @@
-
+//Floyd's Tortoise and Hare Algo
+//https://leetcode.com/problems/find-the-duplicate-number/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
+        do{
+            hare = nums[nums[hare]];
+            tortoise = nums[tortoise];
+        }while(tortoise !=hare);
+        tortoise = nums[0];
+        while(tortoise!=hare){
+            hare = nums[hare];
+            tortoise = nums[tortoise];
+        }
+        return hare;
+    }
+};
